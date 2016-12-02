a97253fa4d0f97aab4355bde2bd8846c6b68a588
Robb <Robb@192.168.1.21>
2016-12-02 13:56:47 +0100

    Version 2.0 Ejercicio Herencia7

---
+++ Herencia7/src/herencia7/diHola.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia7;
+
+/**
+ *
+ * @author Robb
+ */
+public class diHola {
+    
+    public void decirHola(){
+        System.out.println("Hola");
+    }
+}
